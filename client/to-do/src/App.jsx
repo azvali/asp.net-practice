56c0ae3f5ca37316e75bee1782155320cd5b6948
Yousef <yousefm2315@gmail.com>
2025-06-22 00:22:29 -0400

    front end mostly finished

--- client/to-do/src/App.jsx
+++ client/to-do/src/App.jsx
@@ -1,6 +1,18 @@
-import './App.css'
+import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+
+  const [task, setTask] = useState('');
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    //do something
+    console.log(task);
+    setTask('');
+  }
 
 
   return (
@@ -9,11 +21,12 @@ function App() {
         <h1>To do list</h1>
       </div>
       <form className='input-field'>
-        <input className='todo-info' type='text' placeholder='Input Task'></input>
-        <button type='submit'>Add</button>
+        <input className='todo-info' type='text' placeholder='Input Task' value={task} onChange={(e) => {setTask(e.target.value)}}></input>
+        <button type='submit' onClick={(e) => {handleSubmit(e)}}>Add</button>
       </form>
       <div className='list-items'>
-
+        <h1> temp </h1>
+        <button>Remove</button>
       </div>
     </>
   )
